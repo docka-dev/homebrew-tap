66e919e89f82a9d37466501aaf9401bfe55b6ab8
sanix-darker <s4nixd@gmail.com>
2026-01-06 09:04:28 +0100

    chore: update to v0.0.3

--- Formula/docka.rb
+++ Formula/docka.rb
@@ -4,38 +4,33 @@
 class Docka < Formula
   desc "Self-hosted cloud infrastructure management platform"
   homepage "https://docka.dev"
-  version "1.0.0"
+  version "0.0.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/docka-dev/docka-releases/releases/download/v#{version}/docka-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
+      sha256 "3aa8bed8fd737f0abc449ae9d64bd6bada133b03a3e769d4908cebb001233a65"
     end
     on_intel do
       url "https://github.com/docka-dev/docka-releases/releases/download/v#{version}/docka-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
+      sha256 "4f32b92b5c34bc548a26143756465988334e4a6882fe6a4378d039c683aca7b0"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/docka-dev/docka-releases/releases/download/v#{version}/docka-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "c76ccde56f8f9fc1b6d798253c8d1861fd9b57f0c6309063bb25aab45a56f766"
     end
     on_intel do
       url "https://github.com/docka-dev/docka-releases/releases/download/v#{version}/docka-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+      sha256 "cc86de91d94220fe22c8ec60c5e303b9007390759d1fe8fbf4a62471e96b2b24"
     end
   end
 
   def install
     bin.install "docka"
-    bin.install "docka-agent"
-    bin.install "docka-cli"
-
-    # Generate shell completions
-    generate_completions_from_executable(bin/"docka-cli", "completion")
   end
 
   def caveats
@@ -43,13 +38,8 @@ class Docka < Formula
       Docka has been installed!
 
       Quick start:
-        docka serve                    # Start the Docka server
-        docka-cli --help              # CLI commands
-        docka-agent --help            # Agent for managed servers
-
-      Configuration:
-        Default config location: ~/.docka/config.yaml
-        Default data directory: ~/.docka/data
+        docka serve           # Start the Docka server
+        docka --help          # CLI commands
 
       Documentation: https://docka.dev/docs
     EOS
