42fe1e6504604910fc1199e64ff6021756018a57
sanix-darker <s4nixd@gmail.com>
2026-01-06 18:50:47 +0100

    Update to v0.0.3 with distributed binaries (agent, cli, server)

--- Formula/docka.rb
+++ Formula/docka.rb
@@ -10,27 +10,29 @@ class Docka < Formula
   on_macos do
     on_arm do
       url "https://github.com/docka-dev/docka-releases/releases/download/v#{version}/docka-darwin-arm64.tar.gz"
-      sha256 "3aa8bed8fd737f0abc449ae9d64bd6bada133b03a3e769d4908cebb001233a65"
+      sha256 "7852b630a5ce14b2d19fc00f5d01e811c311791e51681de24bea1ef1cc5aa123"
     end
     on_intel do
       url "https://github.com/docka-dev/docka-releases/releases/download/v#{version}/docka-darwin-amd64.tar.gz"
-      sha256 "4f32b92b5c34bc548a26143756465988334e4a6882fe6a4378d039c683aca7b0"
+      sha256 "0e5e356c9d4f9f189f8e12c5eb1b190c3d276c2f048a3faadbf7cd3bb3d14b41"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/docka-dev/docka-releases/releases/download/v#{version}/docka-linux-arm64.tar.gz"
-      sha256 "c76ccde56f8f9fc1b6d798253c8d1861fd9b57f0c6309063bb25aab45a56f766"
+      sha256 "ff2bb75408444b04b36b26458e4fe2828c42f6b78743b5193b099a912d418f0b"
     end
     on_intel do
       url "https://github.com/docka-dev/docka-releases/releases/download/v#{version}/docka-linux-amd64.tar.gz"
-      sha256 "cc86de91d94220fe22c8ec60c5e303b9007390759d1fe8fbf4a62471e96b2b24"
+      sha256 "918fdd7c76652193a0c10aec908c4074033aed1bd1d2d81af568a0df67d3e9b6"
     end
   end
 
   def install
     bin.install "docka"
+    bin.install "docka-server"
+    bin.install "docka-agent"
   end
 
   def caveats
